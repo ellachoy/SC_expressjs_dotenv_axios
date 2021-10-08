b3aa0d2bfe0e4f9a761ac54e95a8275a763a170c
Ella Choy <egal21de@gmail.com>
2021-10-08 17:44:03 +0200

    test2

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const port= process.env.PORT || 3000;
 
 // app.set('views', './views')
 //view engine
-// app.set('view engine', 'ejs');
+ app.set('view engine', 'ejs');
 
 
 app.listen(port, ()=>{
     console.log(`listening at http://localhost:${port}`)
 })
 
-app.get('/index', (req, res) => {
-    res.render('index.ejs',{title:home})
+app.get('/', (req, res) => {
+    res.render('index.ejs',{title:"home"})
 })
 app.get('/about', (req, res) => {
    res.render('about.ejs')
